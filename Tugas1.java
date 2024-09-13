13f471a9f68304733a202c33edb0fd8b612d4efa
Chinta Yanggest Br Tarigan <chintacapric836@gmail.com>
2024-09-13 08:47:18 +0700

    first commit

---
+++ Tugas1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Tugas1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan Namamu\t: ");
+        String nama = scanner.nextLine();
+        System.out.print("Masukkan Nimmu\t: ");
+        int nim = scanner.nextInt();
+
+        System.out.print("Hi "+nama);
+        System.out.print(" "+nim);
+    }
+}
