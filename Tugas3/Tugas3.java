d371a5dcbc35f9acd9e941d03fac634293a37cd2
Chinta Yanggest Br Tarigan <chintacapric836@gmail.com>
2024-11-13 19:44:16 +0700

    Tugas_3

--- Tugas3/Tugas3.java
+++ Tugas3/Tugas3.java
@@ -3,7 +3,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
-public class TugasFF {
+public class Tugas3 {
     public static void main(String[] args) {
         // Build Frame
         JFrame frame = new JFrame("Sign Up Form");
